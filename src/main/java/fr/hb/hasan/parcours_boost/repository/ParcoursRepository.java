2d7600986a8cd4ea0bfe1b6b5a24ea62909740d1
hasanbasricaglayan <caglihas@gmail.com>
2024-07-20 17:19:05 +0200

    Add custom queries in ParcoursRepository

--- src/main/java/fr/hb/hasan/parcours_boost/repository/ParcoursRepository.java
+++ src/main/java/fr/hb/hasan/parcours_boost/repository/ParcoursRepository.java
@@ -2,6 +2,16 @@ package fr.hb.hasan.parcours_boost.repository;
 
 import fr.hb.hasan.parcours_boost.business.Parcours;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ParcoursRepository extends JpaRepository<Parcours, Long> {
+	List<Parcours> findAllByOrderByNomAsc();
+
+	@Query("SELECT p FROM Parcours AS p WHERE p.nom LIKE '%place%'")
+	List<Parcours> findByNomContaining();
+
+	@Query("SELECT DISTINCT p FROM Parcours AS p INNER JOIN p.positions AS pos WHERE pos.longitude > 45.75")
+	List<Parcours> findByPositionGreaterThan();
 }
